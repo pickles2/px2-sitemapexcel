ef9055b075509c950563ae406e72708ffb943725
Tomoya Koyanagi <tomk79@gmail.com>
2017-04-14 21:43:15 +0900

    (テストコード追加)

--- tests/csv2xlsxTest.php
+++ tests/csv2xlsxTest.php
@@ -20,7 +20,7 @@ class csv2xlsxTest extends PHPUnit_Framework_TestCase{
 	}
 
 	/**
-	 * *.xlsx to .csv 変換のテスト
+	 * `*.xlsx` to `*.csv` 変換のテスト
 	 */
 	public function testXlsx2CsvConvert(){
 
@@ -36,15 +36,62 @@ class csv2xlsxTest extends PHPUnit_Framework_TestCase{
 		$this->fs->mkdir(__DIR__.'/testData/files/dist/');
 
         $px2_sitemapexcel = new \tomk79\pickles2\sitemap_excel\pickles_sitemap_excel($px);
-        $px2_sitemapexcel->csv2xlsx( __DIR__.'/testData/files/test1.csv', __DIR__.'/testData/files/dist/test1.xlsx' );
-		$this->assertTrue( is_file( __DIR__.'/testData/files/dist/test1.xlsx' ) );
+        $px2_sitemapexcel->csv2xlsx( __DIR__.'/testData/files/custom_column.csv', __DIR__.'/testData/files/dist/custom_column.xlsx' );
+		$this->assertTrue( is_file( __DIR__.'/testData/files/dist/custom_column.xlsx' ) );
 
+		// 値をチェック
+		$objPHPExcel = \PHPExcel_IOFactory::load( __DIR__.'/testData/files/dist/custom_column.xlsx' );
+		$objPHPExcel->setActiveSheetIndex(0);
+		$objSheet = $objPHPExcel->getActiveSheet();
+		$this->assertEquals( $objSheet->getCell('V8')->getCalculatedValue(), 'custom1' );
+		$this->assertEquals( $objSheet->getCell('W8')->getCalculatedValue(), 'custom2' );
+		$this->assertEquals( $objSheet->getCell('V9')->getCalculatedValue(), 'home-1' );
+		$this->assertEquals( $objSheet->getCell('W9')->getCalculatedValue(), 'home-2' );
 
 		chdir($cd);
 		$px->__destruct();// <- required on Windows
 		unset($px);
 
 	}//testXlsx2CsvConvert()
+
+	/**
+	 * 親ページがないページの変換テスト
+	 */
+	public function testHasNoParentConvert(){
+
+		$cd = realpath('.');
+		chdir(__DIR__.'/testData/standard/');
+
+		$px = new picklesFramework2\px('./px-files/');
+		$toppage_info = $px->site()->get_page_info('');
+		// var_dump($toppage_info);
+		// $this->assertEquals( $toppage_info['title'], '<HOME>' );
+		// $this->assertEquals( $toppage_info['path'], '/index.html' );
+
+		$this->fs->mkdir(__DIR__.'/testData/files/dist/');
+
+        $px2_sitemapexcel = new \tomk79\pickles2\sitemap_excel\pickles_sitemap_excel($px);
+        $px2_sitemapexcel->csv2xlsx( __DIR__.'/testData/files/has_no_parent.csv', __DIR__.'/testData/files/dist/has_no_parent.xlsx' );
+		$this->assertTrue( is_file( __DIR__.'/testData/files/dist/has_no_parent.xlsx' ) );
+
+		// 値をチェック
+		$objPHPExcel = \PHPExcel_IOFactory::load( __DIR__.'/testData/files/dist/has_no_parent.xlsx' );
+		$objPHPExcel->setActiveSheetIndex(0);
+		$objSheet = $objPHPExcel->getActiveSheet();
+		$this->assertEquals( $objSheet->getCell('V8')->getCalculatedValue(), 'custom1' );
+		$this->assertEquals( $objSheet->getCell('W8')->getCalculatedValue(), 'custom2' );
+		$this->assertEquals( $objSheet->getCell('V9')->getCalculatedValue(), 'home-1' );
+		$this->assertEquals( $objSheet->getCell('W9')->getCalculatedValue(), 'home-2' );
+		$this->assertEquals( $objSheet->getCell('A15')->getCalculatedValue(), 'EndOfData' );
+
+		chdir($cd);
+		$px->__destruct();// <- required on Windows
+		unset($px);
+
+	}//testHasNoParentConvert()
+
+
+
 
 	/**
 	 * `.px_execute.php` を実行し、標準出力値を返す
